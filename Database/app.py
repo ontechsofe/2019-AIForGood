788cc53807a58daa71aa2c36264dd2dc15add1a2
Matthew Boivin <31860965+MattyB97@users.noreply.github.com>
2019-03-22 20:32:21 -0400

    Added Migration and Category Resources

--- Database/app.py
+++ Database/app.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources.Database import Database
+from resources.hello import hello
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
