320893063bdcba2005e2ceb19d2c24c0ab986f89
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-03-23 04:35:00 -0400

    send server

--- server/vision/app.py
+++ server/vision/app.py
@@ -21,9 +21,7 @@ def vision():
     data['can_id'] = request.form['can_id']
     data['trash_item'] = prediction(request.form['image'])
     json_data = json.dumps(data)
-
-
-
+    send_server(json_data)
     return json_data
 
 
@@ -47,16 +45,21 @@ def prediction(b64):
             fh.write(base64.b64decode(b64[1]))
 
         f = open('temp.hide.jpg', 'rb', buffering=0)
-        
-        conn.request("POST", "/customvision/v1.0/Prediction/14375ed8-f31f-4115-ace3-6c70d2eabcf3/image?%s" %
+
+        conn.request('POST', '/customvision/v1.0/Prediction/14375ed8-f31f-4115-ace3-6c70d2eabcf3/image?%s' %
                      params, f.readall(), headers)
         response = conn.getresponse()
-        data = json.loads(response.read().decode("utf-8"))
-
+        data = json.loads(response.read().decode('utf-8'))
+        conn.close()
         return data['Predictions'][0]['Tag']
     except Exception as e:
         print(e)
 
+def send_server(json_data):
+    conn = http.client.HTTPConnection(
+        '127.0.0.1', port=5001)
+    conn.request('POST', '/api/Waste', json_data)
+    conn.close()
 
 if __name__ == '__main__':
     app.run(port=5000)
