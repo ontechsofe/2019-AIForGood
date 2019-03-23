99fc1f7cf6ed83091719d3edc09ef3172143393d
Matthew Boivin <31860965+MattyB97@users.noreply.github.com>
2019-03-22 23:09:19 -0400

    Bye Bye Everything

--- Database/resources/hello.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-
-class hello(Resource):
-    def get(self):
-        return {"message": "Hello, World!"}
-
-    def post(self):
-        return {"message": "Hello, World!"}
