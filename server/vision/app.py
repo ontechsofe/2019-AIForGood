a4ba273c3eeb5197960d43b07d4bf13dc76831ea
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-03-23 01:43:31 -0400

    remove garbage route

--- server/vision/app.py
+++ server/vision/app.py
@@ -1,15 +1,19 @@
 from flask import Flask, request
-import  io, http.client, urllib.request, urllib.parse, urllib.error, base64, json
+import io
+import http.client
+import urllib.request
+import urllib.parse
+import urllib.error
+import base64
+import json
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def root():
     return 'Welcome to root directory.'
 
-@app.route('/not/<hi>')
-def not_root(hi):
-    return f'Welcome to root directory. {hi}'
 
 @app.route('/vision', methods=['POST'])
 def vision():
@@ -17,20 +21,22 @@ def vision():
     # print(request.form['image'])
     return prediction(request.form['image'])
 
+
 def prediction(b64):
     headers = {
-        # Request headers 
-        'Content-Type': 'multipart / form-data', 
-        'Prediction-key': '4128a622079d4a5381c7a9267528a750', 
+        # Request headers
+        'Content-Type': 'multipart / form-data',
+        'Prediction-key': '4128a622079d4a5381c7a9267528a750',
     }
 
-    params = urllib.parse.urlencode({ 
-        # Request parameters 
-        'iterationId': '5c3926ab-6c78-4b87-b150-6487bfeab75a', 
+    params = urllib.parse.urlencode({
+        # Request parameters
+        'iterationId': '5c3926ab-6c78-4b87-b150-6487bfeab75a',
     })
 
-    try : 
-        conn = http.client.HTTPSConnection('southcentralus.api.cognitive.microsoft.com') 
+    try:
+        conn = http.client.HTTPSConnection(
+            'southcentralus.api.cognitive.microsoft.com')
         # print(b64)
         # f = open('data/JPEG_20190322_175157.jpg', 'rb', buffering=0)
         # b64 = base64.b64encode(f.read()).split(',')
@@ -38,17 +44,18 @@ def prediction(b64):
         b64 = b64.split(',')
         with open('temp.jpg', "wb") as fh:
             fh.write(base64.b64decode(b64[1]))
-        
+
         f = open('temp.jpg', 'rb', buffering=0)
 
-        conn.request("POST" , "/customvision/v1.0/Prediction/14375ed8-f31f-4115-ace3-6c70d2eabcf3/image?%s" % params, f.readall(), headers) 
+        conn.request("POST", "/customvision/v1.0/Prediction/14375ed8-f31f-4115-ace3-6c70d2eabcf3/image?%s" %
+                     params, f.readall(), headers)
         response = conn.getresponse()
         data = response.read()
         # data = json.load(data)
         conn.close()
         return data
-    except Exception as e: 
-        # print ("[Errno {0}] {1}". format (e. errno, e. strerror)) 
+    except Exception as e:
+        # print ("[Errno {0}] {1}". format (e. errno, e. strerror))
         print(e)
 
 
