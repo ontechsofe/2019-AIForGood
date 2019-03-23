48cf6fafdf780bdc77fd17229680382e36fa0261
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-03-23 07:10:13 -0400

    remove things

--- database/app.py
+++ database/app.py
@@ -1,17 +1,5 @@
 from flask import Flask
 
-def create_app(config_filename):
-    app = Flask(__name__)
-    app.config.from_object(config_filename)
-    
-    from app import api_bp
-
-    from Model import db
-    db.init_app(app)
-
-    return app
-
-
 if __name__ == "__main__":
     app = create_app("config")
     app.run(port=5001, debug=True)
